43cf5eebdd07993f1d7eaa81ec63ff60cad6d359
Adam Kmet <adamkmet97@gmail.com>
2019-04-14 17:15:55 +0100

    redesigning home page

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -4,6 +4,27 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Entity\Polling;
+//use App\Entity\Support;
+//use App\Repository\SupportRepository;
+use App\Entity\Supporting;
+use App\Repository\SupportingRepository;
+use Doctrine\ORM\Mapping\Id;
+use Symfony\Component\Security\Core\User\UserInterface;
+use App\Entity\Vote;
+use App\Entity\User;
+use App\Entity\Comment;
+use App\Repository\CommentRepository;
+use App\Form\PollingType;
+use App\Repository\PollingRepository;
+use Doctrine\ORM\EntityManager;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use App\Repository\VoteRepository;
+use App\Repository\UserRepository;
+use App\Form\CommentType;
+use App\Form\VoteType;
+use Doctrine\ORM\Query;
 
 class DefaultController extends AbstractController
 {
